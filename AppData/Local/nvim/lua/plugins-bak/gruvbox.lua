c98f2da41b68bb1647775d95294543f8ffc184d3
Itay Blokh <itay.blokh@dart.com.sg>
2025-06-12 10:33:34 +0800

    Delete AppData/Local/nvim directory

--- AppData/Local/nvim/lua/plugins-bak/gruvbox.lua
+++
@@ -1,9 +0,0 @@
-local add = MiniDeps.add
-
-add({
-source = "sainnhe/gruvbox-material",
-})
-
-
-
-vim.cmd("colorscheme gruvbox-material")
--- AppData/Local/nvim/lua/plugins-bak/which-key.lua
+++
@@ -1,9 +0,0 @@
-local add = MiniDeps.add
-
-add({
-source = "folke/which-key.nvim",
-})
-
-require("which-key").setup({
-	delay = 1500,
-})
